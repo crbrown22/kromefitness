4d60ac4f83a135e445e47f3828226a8033741392
Christopher Brown <152041312+crbrown22@users.noreply.github.com>
2024-10-22 16:16:58 -0500

    Update settings.py

--- kromefitness/settings.py
+++ kromefitness/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kromefitness@gmail.com'
-EMAIL_HOST_PASSWORD = 'gcvxpvfkhshvqtel'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
